0e970a4a4cd9c9c657d58b025d52c4571127442c
mehmetcanari <bsn.mehmetcanari@gmail.com>
2023-04-21 14:55:39 +0300

    player char rotate via indicator

---
+++ Assets/Scripts/Input/CharacterRotating.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace Chaos.Escape
+{
+    public class CharacterRotating : MonoBehaviour
+    {
+        #region INSPECTOR FIELDS
+
+        [SerializeField] private Transform indicatorCore;
+        [SerializeField] private Transform characterMesh;
+        
+        #endregion
+
+        #region UNITY METHODS
+
+        private void Update()
+        {
+            ForceLookToIndicatorRotationY();
+        }
+
+        #endregion
+        
+        #region PRIVATE METHODS
+
+        private void ForceLookToIndicatorRotationY()
+        {
+            var indicatorRotationY = indicatorCore.rotation.eulerAngles.y;
+            characterMesh.rotation = Quaternion.Euler(0, indicatorRotationY, 0);
+        }
+
+        #endregion
+    }
+}
